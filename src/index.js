92b66b4e77ac57148e269295b47216e5f6c1b083
saptarshisarkar20 <saptarshi2017cob@gmail.com>
2022-12-18 13:11:16 +0530

    learned partials and 404 page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const hbs = require("hbs");
 
 const express = require("express");
 const app = express();
@@ -11,21 +12,34 @@ const port = 8000;
 // console.log(path.join(__dirname, "../public"));
 const staticPath = path.join(__dirname, "../public");
 const templatePath = path.join(__dirname, "../templates");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //* BUILTIN MIDDLEWARE
 // app.use(express.static(staticPath));
 
 //* SET VIEW ENGINE
 app.set("view engine", "hbs");
-app.set("views", templatePath);
+app.set("views", viewPath);
+
+//* SETTING UP PARTIALS
+hbs.registerPartials(partialsPath);
 
 //* TEMPLATE WITH VIEW ENGINE
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { myName: "Sarkar" });
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
+});
+
+app.get("/about/*", (req, res) => {
+  res.render("404", { type: "inside about" });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", { type: "" });
 });
 
 //* ROUTING TO HOME PAGE
